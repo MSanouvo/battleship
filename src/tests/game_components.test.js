2110328601d5ae0f81c22808869c153d2b637482
Msanouvo <43829596+MSanouvo@users.noreply.github.com>
2024-12-18 02:41:59 -0800

    feat: added function to attack and record tiles
    - clicking on a tile now records tile and checks
    if ship is on tile
    - clicking a repeated tile will log a message and
    prevent further action
    - class style added to played tiles
    - some refactoring done to how board functionality
    operates

--- src/tests/game_components.test.js
+++ src/tests/game_components.test.js
@@ -19,11 +19,6 @@ test('Make sure board Y dimension is right (10)', ()=>{
     expect(game.generateBoard()[0].length).toBe(10)
 })
 
-test('Check that a ship is placed at location', ()=>{
-    const ship = 'ship'
-    expect(game.placeShip(4, 6, ship)).toEqual(['ship'])
-})
-
 test('Check if player loses ship', ()=>{
     const weakShip = new game.Ship('weak', 1)
     const player = new game.Player()
@@ -40,4 +35,27 @@ test('Check if player has no more ships', ()=>{
 test('Check if player move is added to played array', ()=>{
     const player = new game.Player()
     expect(player.addToPlayed(1, 6)).toEqual([[1, 6]])
+})
+
+test('Check that a ship is placed at location', ()=>{
+    const ship = 'ship'
+    const player = new game.Player()
+    player.board = game.generateBoard()
+    expect(game.placeShip(player, 4, 6, ship)).toEqual(['ship'])
+})
+
+test('Check if attack at (4, 6) is valid', ()=>{
+    const player = new game.Player()
+    const ship = new game.Ship()
+    player.board = game.generateBoard()
+    game.placeShip(player, 4, 6, ship)
+    expect(game.receiveAttack(player, 4, 6)).toBe(true)
+})
+
+test('Check if attack at (1, 2) misses', ()=>{
+    const player = new game.Player()
+    const ship = new game.Ship()
+    player.board = game.generateBoard()
+    game.placeShip(player, 4, 6, ship)
+    expect(game.receiveAttack(player, 1, 2)).toBe(false)
 })
