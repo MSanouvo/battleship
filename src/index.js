e8cd348a042c2849c37c5dda681c7d0c5dc3f478
MSanouvo <43829596+MSanouvo@users.noreply.github.com.>
2024-12-19 18:50:37 -0800

    refactor: clean up game manager module

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import "./style.css"
-import { renderBoards, renderShip1, renderShip2 } from "./js_modules/game_manager"
+import { gameManager, renderShip1, renderShip2 } from "./js_modules/game_manager"
 import { Player } from "./js_modules/game_components"
 
+const game = gameManager()
 const player1 = new Player('Player 1')
 player1.ship_count = 1
 export const player2 = new Player('Opponent')
 
 const board_1 = document.querySelector('#board_1')
 const board_2 = document.querySelector('#board_2')
-renderBoards(player1, board_1, false)
-export const opponentBoard = renderBoards(player2, board_2)
+game.renderBoards(player1, board_1, false)
+export const opponentBoard = game.renderBoards(player2, board_2)
 
 renderShip1(player1, board_1)
 renderShip2(player2, board_2)
